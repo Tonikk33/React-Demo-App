8852fc01c813f25a708e05d97611531b5c54df9a
Georgi Pantov <g.pantov@abv.bg>
2019-02-05 09:59:36 +0200

    Logo added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,38 @@
-import React from "react";
-import Aux from "../../hoc/Aux";
-import classes from "./Layout.css"
+import React, { Component } from 'react';
 
-const layout = (props) => (
+import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-    <Aux>
-    <div>Toolbar, SideDrawer,Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
 
-    </Aux>
+    sideDrawerClosedHandler = () => {
+        this.setState( { showSideDrawer: false } );
+    }
 
-);
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        } );
+    }
 
-export default layout;
+    render () {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
+
+export default Layout;
